1d9e80d5d6ac35eadbb05764e1a08877e9811b14
windless <windless.d@gmail.com>
2016-05-08 15:38:42 +0800

    First commit.

---
+++ app/src/main/java/com/learn/wangtianyuan/providertest/MainActivity.java
@@ -0,0 +1,80 @@
+package com.learn.wangtianyuan.providertest;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String newId;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //add data
+        findViewById(R.id.add_data).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("content://com.learn.wangtianyuan.databasetest.provider/book/");
+                ContentValues values = new ContentValues();
+                values.put("name", "A Clash of Kings");
+                values.put("author", "George Martin");
+                values.put("pages", 1040);
+                values.put("price", 22.85);
+                Uri newUri = getContentResolver().insert(uri,values);
+                newId = newUri.getPathSegments().get(1);
+            }
+        });
+
+        //query data
+        findViewById(R.id.query_data).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("content://com.learn.wangtianyuan.databasetest.provider/book/" + newId);
+                Cursor cursor = getContentResolver().query(uri,null,null,null,null);
+                if (null != cursor) {
+                    while (cursor.moveToNext()) {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+
+                        Log.d("MainActivity", "book name is " + name);
+                        Log.d("MainActivity", "book author is " + author);
+                        Log.d("MainActivity", "book pages is " + pages);
+                        Log.d("MainActivity", "book price is " + price);
+                    }
+                    cursor.close();
+                }
+
+            }
+        });
+
+        //update
+        findViewById(R.id.update_data).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("content://com.learn.wangtianyuan.databasetest.provider/book/" + newId);
+                ContentValues values = new ContentValues();
+                values.put("name", "A Storm of Swords");
+                values.put("pages", 1216);
+                values.put("price", 24.05);
+                getContentResolver().update(uri,values,null,null);
+            }
+        });
+
+        //delete data
+        findViewById(R.id.delete_data).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("content://com.learn.wangtianyuan.databasetest.provider/book/" + newId);
+                getContentResolver().delete(uri,null,null);
+            }
+        });
+    }
+}
